7ef49bb120c3776f0a7c2b87ff6b1dd2ee8a1d71
shunrai <2823124008@qq.com>
2026-06-24 07:41:37 +0800

    Enhance world module by adding new imports for interaction, effects, and management components.

---
+++ src/js/world/enemies/enemy-manager.js
@@ -0,0 +1,4 @@
+import * as THREE from 'three'
+import Experience from '../../experience.js'
+import EntityCollisionSystem from '../entity-collision.js'
+import Zombie, { ZombieState } from './zombie.js'
